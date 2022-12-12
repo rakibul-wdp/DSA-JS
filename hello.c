5b6ebbab3dd9d6e9cfc20fd11fb36a0a88574c1c
rakibul-wdp <rakibul.wdp@gmail.com>
2022-12-13 05:25:20 +0600

    again hello world with c

--- hello.c
+++ hello.c
@@ -1,24 +1,7 @@
 #include <stdio.h>
 
-// int main ()
-// {
-//   printf("Hello fucking c World...!!!");
-
-//   return 0;
-// }
-
-int main()
-{
-  int a;
-  int b;
-  int sum;
-
-  a = 50;
-  b = 60;
-
-  sum = a + b;
-
-  printf("Sum is %d", sum);
-
+int main() {
+  printf("hello world \n for learning DSA \n learn C");
+  printf("I'm learning C");
   return 0;
 }
