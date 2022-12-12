6f60a1d1a9337ec5a127eb33ca0f986478899348
rakibul-wdp <rakibul.wdp@gmail.com>
2022-12-13 05:35:20 +0600

    time complexity topic

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Learning C Programming
+# Learning C & DSA
+
+### Data Structure and Algorithm
+
+#### Time Complexity
+
+- Assignment operation -> x = 10, o(1) Big o
+- Comparison operation -> a > b, o(1) Big o
+- Mathematical operation -> 2 + 6, o(1) Big o
+- Function call -> r = f(n)
+- work inside of function
--- hello.c
+++ hello.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 
 int main() {
-  printf("hello world \n for learning DSA \n learn C");
-  printf("I'm learning C");
+  // this is a comment
+  // printf("hello world \n for learning DSA \n learn C");
+  // printf("I'm learning C \n");
+
+  // create variables
+  int num = 15;
+  float floatNum = 32.89;
+  char charNum = "a";
+
+  // print variables
+  printf("%d\n", num);
+  printf("%f\n", floatNum);
+  printf("%c\n", charNum);
+
   return 0;
 }
+
+// int num = 123;
+// printf("%d", num);
+
+// declare a variable
+// int myNum;
+
+// assign a value to the variable
+// myNum = 15;
