8172bb4bde5fbde67403f6cd62c1dd5a7e436781
rakibul-wdp <rakibul.wdp@gmail.com>
2022-12-13 05:55:20 +0600

    big o time complexity

---
+++ timeComplexity.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int n1, n2, result;
+
+  n1 = 10;
+  n2 = 20;
+  result = n1 + n2;
+
+  return 0;
+}
+
+/*
+here is 3 assignment operation and 1 addition operation.
+total 4 operation so O(4) not, it's O(1)
+*/
