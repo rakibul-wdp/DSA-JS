f602ed76a14f27be424bbbdb586af6376510c705
rakibul-wdp <rakibul.wdp@gmail.com>
2022-12-14 04:05:20 +0600

    big o time complexity with another example

--- timeComplexity.c
+++ timeComplexity.c
@@ -11,6 +11,23 @@ int main() {
 }
 
 /*
-here is 3 assignment operation and 1 addition operation.
+here is 3 assignment operation and 1 mathematical operation.
 total 4 operation so O(4) not, it's O(1)
+*/
+
+int main() {
+  int n, result;
+
+  scanf("%d", &n);
+
+  result = n * (n + 1) / 2;
+
+  printf("result = %d\n", result);
+
+  return 0;
+}
+
+/*
+here is 3 mathematical operation and 1 assignment operation.
+total 4 operation and it's O(1), because never change value of n
 */
