a979aae4a8e8a3e6c0189c4bffec410ad2d3ef43
rakibul-wdp <rakibul.wdp@gmail.com>
2022-12-14 04:35:20 +0600

    n square or O(n2)

--- timeComplexity.c
+++ timeComplexity.c
@@ -51,3 +51,23 @@ int main() {
 /*
 here complexity depend on n that's why it's linier complexity, order of n complexity O(n)
 */
+
+int main() {
+  int i, j, n, count;
+
+  scanf("%d", &n);
+
+  count = 0;
+
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n; j++) {
+      count = count + 1;
+    }
+  }
+
+  printf("n = %d, count = %d\n", n, count);
+
+  return 0;
+}
+
+// here time complexity is O(n square)
