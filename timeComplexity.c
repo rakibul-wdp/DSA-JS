5e87e4128f8dcda975e6134ab14001119df989af
rakibul-wdp <rakibul.wdp@gmail.com>
2022-12-14 04:55:20 +0600

    n cube or O(n3)

--- timeComplexity.c
+++ timeComplexity.c
@@ -71,3 +71,24 @@ int main() {
 }
 
 // here time complexity is O(n square)
+
+int main() {
+  int i, j, k, n, count;
+
+  scanf("%d", &n);
+
+  count = 0;
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n; j++) {
+      for (k = 0; k < n; k++) {
+        count = count + 1;
+      }
+    }
+  }
+
+  printf("n = %d, count = %d\n", n, count);
+
+  return 0;
+}
+
+// here time complexity is O(n cube)
