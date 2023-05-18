b6cfec3463718d6fe366c75941db4ced59d135da
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-19 04:05:00 +0600

    basic booleans

---
+++ basic-syntax-variables-data-types/booleans.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+  bool isProgrammingFun = false;
+  bool isFishTasty = true;
+
+  printf("%d\n", isProgrammingFun);
+  printf("%d", isFishTasty);
+
+  return 0;
+}
