82ef7849035b96c40d8c5b24443163b16db4a085
rakibul-wdp <rakibul.wdp@gmail.com>
2023-05-29 04:25:00 +0600

    switch statement

---
+++ basic-syntax-variables-data-types/switch.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+  int day = 2;
+
+  switch (day) {
+    case 1:
+      printf("Monday");
+      break;
+
+    case 2:
+      printf("Tuesday");
+      break;
+    
+    case 3:
+      printf("Wednesday");
+      break;
+  }
+
+  return 0;
+}
+
+/**
+ * 
+*/
