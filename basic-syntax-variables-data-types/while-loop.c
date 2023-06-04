675868056ea720ea06887ebaee471886fd8fa4e1
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-05 04:55:00 +0600

    basic of string

--- basic-syntax-variables-data-types/while-loop.c
+++ basic-syntax-variables-data-types/while-loop.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
 
 int main() {
-  int matrix[2][3] = {{1, 4, 2}, {3, 6, 8}};
+  char greetings[] = "Hello World";
 
-  int i, j;
+  printf("%s", greetings);
 
-  for (i = 0; i < 2; i++) {
-    for (j = 0; j < 3; j++) {
-      printf("%d\n", matrix[i][j]);
-    }
-  }
-
-    return 0;
+  return 0;
 }
