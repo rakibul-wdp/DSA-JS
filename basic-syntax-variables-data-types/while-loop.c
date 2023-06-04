3dd68eae2bb34075d303d4321a420d2daceac937
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-05 04:05:00 +0600

    loop through of tow d array

--- basic-syntax-variables-data-types/while-loop.c
+++ basic-syntax-variables-data-types/while-loop.c
@@ -2,9 +2,14 @@
 
 int main() {
   int matrix[2][3] = {{1, 4, 2}, {3, 6, 8}};
-  matrix[0][0] = 9;
 
-  printf("%d", matrix[0][0]);
+  int i, j;
 
-  return 0;
+  for (i = 0; i < 2; i++) {
+    for (j = 0; j < 3; j++) {
+      printf("%d\n", matrix[i][j]);
+    }
+  }
+
+    return 0;
 }
