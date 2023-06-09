f40ee9631ddc4c8c1aeaf482899179dbfc19bf6f
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-10 04:55:00 +0600

    use single quotes and backslash

--- basic-syntax-variables-data-types/while-loop.c
+++ basic-syntax-variables-data-types/while-loop.c
@@ -2,8 +2,12 @@
 
 int main() {
   char txt[] = "We ar the \"Vikings\" from the north.";
+  char txt2[] = "It\'s alright";
+  char text3[] = "The character \\ is called backslash";
 
-  printf("%s", txt);
+  printf("%s\n", txt);
+  printf("%s\n", txt2);
+  printf("%s", text3);
 
   return 0;
 }
