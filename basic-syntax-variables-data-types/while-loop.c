4a711b218ec5b5f9676f7bd7aea521a401c6f6bc
rakibul-wdp <rakibul.wdp@gmail.com>
2023-07-02 04:55:00 +0600

    take input by scanf

--- basic-syntax-variables-data-types/while-loop.c
+++ basic-syntax-variables-data-types/while-loop.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 int main() {
-  char str1[] = "Hello";
-  char str2[] = "Hello";
-  char str3[] = "Hi";
+  int myNum;
 
-  printf("%d\n", strcmp(str1, str2));
-  printf("%d", strcmp(str1, str3));
+  printf("Type a number: \n");
+
+  scanf("%d", &myNum);
+
+  printf("Your number is: %d", myNum);
 
   return 0;
 }
