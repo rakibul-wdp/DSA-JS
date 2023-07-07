58f3fd80e844d9325b8acdd10df7885f4281ab9c
rakibul-wdp <rakibul.wdp@gmail.com>
2023-07-08 04:05:00 +0600

    creating pointer

--- basic-syntax-variables-data-types/while-loop.c
+++ basic-syntax-variables-data-types/while-loop.c
@@ -2,8 +2,13 @@
 
 int main() {
   int myAge = 23;
+  int *ptr = &myAge;
 
-  printf("%p", &myAge);
+  printf("%d\n", myAge);
+
+  printf("%p\n", &myAge);
+
+  printf("%p\n", ptr);
 
   return 0;
 }
