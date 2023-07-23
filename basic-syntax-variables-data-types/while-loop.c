d2dae1d9caa7dbb2040048d36809128a1114841a
rakibul-wdp <rakibul.wdp@gmail.com>
2023-07-24 04:55:00 +0600

    recive multiple paramenter in a function

--- basic-syntax-variables-data-types/while-loop.c
+++ basic-syntax-variables-data-types/while-loop.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-void myFunction(char name[]) {
-  printf("Hello %s\n", name);
+void myFunction(char name[], int age) {
+  printf("Hello %s. You are %d years old. \n", name, age);
 }
 
 int main() {
-  myFunction("Abul");
-  myFunction("Babul");
-  myFunction("Cabul");
+  myFunction("Abul", 10);
+  myFunction("Babul", 11);
+  myFunction("Cabul", 12);
 
   return 0;
 }
