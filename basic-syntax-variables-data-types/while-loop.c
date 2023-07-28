17ff10d4c85f9a6e1b09e21e2d017e627ed76a62
rakibul-wdp <rakibul.wdp@gmail.com>
2023-07-29 04:05:00 +0600

    return value from function with variable

--- basic-syntax-variables-data-types/while-loop.c
+++ basic-syntax-variables-data-types/while-loop.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int myFunction(int x) {
-  return 5 + x;
+int myFunction(int x, int y) {
+  return x + y;
 }
 
 int main() {
-  printf("Result is: %d", myFunction(3));
+  printf("Result is: %d", myFunction(5, 4));
 
   return 0;
 }
