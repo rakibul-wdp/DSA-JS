96184de82ad733446a36edee6c9ed2f6e0b59aaf
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-14 05:56:06 +0600

    take two inputs

--- output.c
+++ output.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main() {
-  int abul;
-  scanf("%d", &abul);
-  printf("%d", abul);
+  int abul, babul;
+  scanf("%d %d", &abul, &babul);
+  printf("%d %d", abul, babul);
 
   return 0;
 }
