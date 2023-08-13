2e1b434e2c41ac1bf3bebdad0ac706bd5ec0f6f5
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-13 09:15:40 +0600

    by \t will take a tab

--- hello.c
+++ hello.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 int main() {
-  printf("Hello %% World \\ \n");
-  printf("Now I'm a programmer");
+  printf("Hello %% World \\ \nI'm a \tdeveloper\nand programmer");
 
   return 0;
 }
+
+// \n - new line
+// \t - tab
