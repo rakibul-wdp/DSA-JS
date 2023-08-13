6d1cdc11b87a509339971bd0226c80cb92efa3a2
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-14 04:36:16 +0600

    print float value specific number after decimal

--- variable.c
+++ variable.c
@@ -2,9 +2,10 @@
 
 int main() {
   int abul = 10;
-  float babul = 10.5;
+  float babul = 100.5;
   char cabul = 'z';
-  printf("%d %f %c", abul, babul, cabul);
+  // printf("%d %f %c", abul, babul, cabul);
+  printf("%0.1f", babul);
 
   return 0;
 }
