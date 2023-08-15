f378d3a768fc17c98ff29b38c820bbb629d6940c
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-16 04:55:00 +0600

    no with function definition

--- basic-syntax-variables-data-types/while-loop.c
+++ basic-syntax-variables-data-types/while-loop.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-int myFunction(int x, int y) {
-  return x + y;
-}
+int myFunction(int x, int y);
 
 int main() {
   int result = myFunction(5, 3);
   printf("Result is = %d", result);
 
   return 0;
+}
+
+int myFunction(int x, int y) {
+  return x + y;
 }
