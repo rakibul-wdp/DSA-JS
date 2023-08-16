82a399f49ce8de9449d3561fee2627867432a792
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-17 04:55:00 +0600

    square root

--- basic-syntax-variables-data-types/while-loop.c
+++ basic-syntax-variables-data-types/while-loop.c
@@ -1,18 +1,7 @@
 #include <stdio.h>
 
-int sum(int k);
-
 int main() {
-  int result = sum(10);
-  printf("%d", result);
+  printf("%f", sqrt(4));
 
   return 0;
-}
-
-int sum(int k) {
-  if (k > 0) {
-    return k + sum(k - 1);
-  } else {
-    return 0;
-  }
 }
