80ee2ec1eb4fe65aab95083d0b5510e0ec88ea46
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-19 04:55:00 +0600

    write sudo code of find the largest number among three numbers

--- DSA/add-two-num.c
+++ DSA/add-two-num.c
@@ -1,24 +1,25 @@
-// Algorithm 1: Add two numbers entered by the user
+// Algorithm 2: Find the largest number among three numbers
 /**
  * Step 1: Start
- * Step 2: Declare variables num1, num2 and sum.
- * Step 3: Read values num1 and num2.
- * Step 4: Add num1 and num2 and assign the result to sum. (sum <- num1 + num2)
- * Step 5: Display sum
- * Step 6: Stop
+ * Step 2: Declare variables a,b and c.
+ * Step 3: Read variables a,b and c.
+ * Step 4: If a > b
+ *            If a > c
+ *               Display a is the largest number.
+ *            Else
+ *              Display c is the largest number.
+ *         Else
+ *           If b > c
+ *              Display b is the largest number.
+ *           Else
+ *             Display c is the greatest number.
+ * Step 5: Stop
 */
 
 #include <stdio.h>
 
 int main() {
-  int num1;
-  int num2;
-
-  scanf("%d %d", &num1, &num2);
-
-  int sum = num1 + num2;
-
-  printf("%d", sum);
+  
 
   return 0;
 }
