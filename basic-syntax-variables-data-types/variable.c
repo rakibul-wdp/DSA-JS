8a3ccf06f438dcfd661b05ad4f9184d709ef4613
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-21 06:02:21 +0600

    type of c variable

--- basic-syntax-variables-data-types/variable.c
+++ basic-syntax-variables-data-types/variable.c
@@ -9,3 +9,7 @@ int main() {
 
   return 0;
 }
+
+// int - stores integers (whole numbers), without decimals, such as 123 or - 123
+// float - stores floating numbers, with decimals, such as 19.99 or -19.99
+// char - stores single characters, such as "a" or "B". Char values ar surrounded by  single quotes
