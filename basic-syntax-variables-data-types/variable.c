80af293eb46fac2ad935c1f4743f1169efbca21c
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-21 06:43:59 +0600

    format specifiers

--- basic-syntax-variables-data-types/variable.c
+++ basic-syntax-variables-data-types/variable.c
@@ -13,3 +13,8 @@ int main() {
 // int - stores integers (whole numbers), without decimals, such as 123 or - 123
 // float - stores floating numbers, with decimals, such as 19.99 or -19.99
 // char - stores single characters, such as "a" or "B". Char values ar surrounded by  single quotes
+
+/**
+ * To output the value of an int variable, use %d or %i surrounded by double quotes
+ * To print other types, use %c for char and %f for float
+*/
