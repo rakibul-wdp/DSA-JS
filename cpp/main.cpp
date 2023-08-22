a29e430f149af5317fc9060d08b06fd792bb73a3
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-23 04:55:00 +0600

    user input string

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -2,15 +2,11 @@
 using namespace std;
 
 int main() {
-  int x, y;
-  int sum;
+  string firstName;
 
-  cin >> x;
-  cin >> y;
+  cin >> firstName;
 
-  sum = x + y;
-
-  cout << sum;
+  cout << firstName;
 
   return 0;
 }
