eb472d383b7665711094ec97bacf5ac320b9503f
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-27 04:55:00 +0600

    nested loops

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -2,8 +2,12 @@
 using namespace std;
 
 int main() {
-  for (int i = 0; i < 5; i++) {
-    cout << i << "\n";
+  for (int i = 1; i <= 2; ++i) {
+    cout << "Outer: " << i << "\n";
+
+    for (int j = 1; j <= 3; ++j) {
+      cout << " Inner: " << j << "\n";
+    }
   }
 
     return 0;
