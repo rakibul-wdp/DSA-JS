2d6cde90f1575c8d71a31a494b1ae0403d6cba6b
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-29 08:32:20 +0600

    break in for loop

--- basic-syntax-variables-data-types/while-loop.c
+++ basic-syntax-variables-data-types/while-loop.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main() {
-  int i, j;
+  int i;
 
-  for (i = 1; i <= 2; ++i) {
-    printf("Outer: %d\n", i);
-
-    for (j = 1; j <= 3; ++j) {
-      printf(" Inner: %d\n", j);
+  for (i = 0; i < 10; i++) {
+    if (i == 4) {
+      break;
     }
+
+    printf("%d\n", i);
   }
 
     return 0;
