41d32b530108a4839e0da5b8bdfd3ece9a82d1fb
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-30 04:40:13 +0600

    multiple structure

--- basic-syntax-variables-data-types/output.c
+++ basic-syntax-variables-data-types/output.c
@@ -7,12 +7,16 @@ struct myStructure {
 
 int main() {
   struct myStructure s1;
+  struct myStructure s2;
 
   s1.myNum = 15;
   s1.myLetter = 'A';
 
-  printf("My number: %d\n", s1.myNum);
-  printf("My letter: %c\n", s1.myLetter);
+  s2.myNum = 20;
+  s2.myLetter = 'C';
+
+  printf("My number: %d\n", s2.myNum);
+  printf("My letter: %c\n", s2.myLetter);
 
   return 0;
 }
