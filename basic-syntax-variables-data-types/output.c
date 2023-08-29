4778f0aed47877ae16bde382cea1c2c5eeec0f05
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-30 04:37:25 +0600

    access structure members

--- basic-syntax-variables-data-types/output.c
+++ basic-syntax-variables-data-types/output.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
+struct myStructure {
+  int myNum;
+  char myLetter;
+};
+
 int main() {
-  double num = 1000.8392839;
-  printf("%lf", num);
+  struct myStructure s1;
+
+  s1.myNum = 15;
+  s1.myLetter = 'A';
+
+  printf("My number: %d\n", s1.myNum);
+  printf("My letter: %c\n", s1.myLetter);
 
   return 0;
 }
