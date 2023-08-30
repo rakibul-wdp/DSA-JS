8bff84414d678f68f7b56ec0228e4f36b5168de6
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-30 06:14:36 +0600

    enums

--- basic-syntax-variables-data-types/output.c
+++ basic-syntax-variables-data-types/output.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 
-struct Car {
-  char brand[50];
-  char model[50];
-  int year;
+enum Level {
+  LOW,
+  MEDIUM,
+  HIGH
 };
 
 int main() {
-  struct Car car1 = {"BMW", "X5", 1999};
-  struct Car car2 = {"Ford", "Mustang", 1969};
-  struct Car car3 = {"Toyota", "Corolla", 2011};
+  enum Level myVar = MEDIUM;
 
-  printf("%s %s %d\n", car1.brand, car1.model, car1.year);
-  printf("%s %s %d\n", car2.brand, car2.model, car2.year);
-  printf("%s %s %d\n", car3.brand, car3.model, car3.year);
+  printf("%d", myVar);
 
   return 0;
 }
