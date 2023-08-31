508addded74570d069f321c3ae88c49ca639e2f8
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-31 10:06:23 +0600

    multiple parameters

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  string letters[2][4] = {
-  { "A", "B", "C", "D" },
-  { "E", "F", "G", "H" }
-};
+void myFunction(string fname, int age) {
+  cout << fname << " Reference. " << age << " years odl. \n";
+}
 
-  cout << letters[0][2];
+int main() {
+  myFunction("Abul", 2);
+  myFunction("Babul", 3);
+  myFunction("Cabul", 4);
 
   return 0;
 }
