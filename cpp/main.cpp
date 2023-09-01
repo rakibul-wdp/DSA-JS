7451a9dec6b76a5a6b04f4709a20dc6130290d33
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-01 08:34:51 +0600

    constructor parameters

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 using namespace std;
 
-class MyClass {
+class Car {
   public:
-    MyClass() {
-      cout << "Hello CPP";
-    }
+    string brand;
+    string model;
+    int year;
+      Car(string x, string y, int z) {
+        brand = x;
+        model = y;
+        year = z;
+      }
 };
 
 int main() {
-  MyClass myObj;
+  Car carObj1("BMW", "X5", 1999);
+  Car carObj2("Ford", "Mustang", 1969);
+
+  cout << carObj1.brand << " " << carObj1.model << " " << carObj1.year << "\n";
+  cout << carObj2.brand << " " << carObj2.model << " " << carObj2.year << "\n";
 
   return 0;
 }
