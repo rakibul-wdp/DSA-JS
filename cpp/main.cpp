b9699fb6d6abc7468c98882b0f85c8c4ef202255
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-02 04:36:35 +0600

    basic try catch in cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,38 +1,19 @@
 #include <iostream>
 using namespace std;
 
-// base class
-class Animal {
-  public:
-    void animalSound() {
-      cout << "The animal makes a sound \n";
-    }
-};
-
-// derived class
-class Pig : public Animal {
-  public:
-    void animalSound() {
-      cout << "The pig says: wee wee \n";
-    }
-};
-
-// derived class
-class Dog : public Animal {
-  public:
-    void animalSound() {
-      cout << "The dog says: bow wow \n";
-    }
-};
-
 int main() {
-  Animal myAnimal;
-  Pig myPig;
-  Dog myDog;
-
-  myAnimal.animalSound();
-  myPig.animalSound();
-  myDog.animalSound();
+  try {
+    int age = 20;
+  if (age >= 18) {
+    cout << "Access granted - you are old enough.";
+  } else {
+    throw(age);
+  }
+  }
+  catch (int myNum) {
+    cout << "Access denied - You must be at least 18 years odl.\n";
+    cout << "Age is: " << myNum;
+  }
 
   return 0;
 }
