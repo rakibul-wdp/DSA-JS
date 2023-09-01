a9a97d674cf3e75f7d3e85305de43066164981f8
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-01 09:14:08 +0600

    access specifiers

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,26 +1,17 @@
 #include <iostream>
 using namespace std;
 
-class Car {
+class MyClass {
   public:
-    string brand;
-    string model;
-    int year;
-    Car(string x, string y, int z);
-};
-
-Car::Car(string x, string y, int z) {
-  brand = x;
-  model = y;
-  year = z;
+    int x;
+  private:
+    int y;
 };
 
 int main() {
-  Car carObj1("BMW", "X5", 1999);
-  Car carObj2("Ford", "Mustang", 1969);
-
-  cout << carObj1.brand << " " << carObj1.model << " " << carObj1.year << "\n";
-  cout << carObj2.brand << " " << carObj2.model << " " << carObj2.year << "\n";
+  MyClass myObj;
+  myObj.x = 25;
+  myObj.y = 50;
 
   return 0;
 }
