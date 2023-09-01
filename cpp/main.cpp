dab003975717be0ca7b68fbd034a3267da217cc7
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-01 08:11:07 +0600

    class methods inside example

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,27 +1,16 @@
 #include <iostream>
 using namespace std;
 
-class Car {
+class MyClass {
   public:
-    string brand;
-    string model;
-    int year;
+    void myMethod() {
+      cout << "Hello World!";
+    }
 };
 
 int main() {
-  Car carObj1;
-  carObj1.brand = "BMW";
-  carObj1.model = "X5";
-  carObj1.year = 1999;
-
-  Car carObj2;
-  carObj2.brand = "Ford";
-  carObj2.model = "Mustang";
-  carObj2.year = 1969;
-
-  cout << carObj1.brand << " " << carObj1.model << " " << carObj1.year << "\n";
-
-  cout << carObj2.brand << " " << carObj2.model << " " << carObj2.year << "\n";
+  MyClass myObj;
+  myObj.myMethod();
 
   return 0;
 }
