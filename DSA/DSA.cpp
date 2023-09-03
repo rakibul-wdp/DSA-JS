9858157e692c0b973e1c620f51bddc4acf69f1fe
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-04 04:53:22 +0600

    write sudo code of find the fac of a num

--- DSA/DSA.cpp
+++ DSA/DSA.cpp
@@ -1,19 +1,16 @@
-// Algorithm 3: Find Roots of a Quadratic Equation ax^2+bx+c=0
+// Algorithm 4: Find the factorial of a number
 /*
 Step 1: Start
-Step 2: Declare variables a, b, c, D, x1, x2, rp and ip;
-Step 3: Calculate discriminant
-        D <- b2-rac
-Step 4: If D >= 0
-                r1 <- (-b+root(D))/2a
-                r2 <- (-b-root(d))/2a
-                Display r1 and r2 as roots.
-        Else
-            Calculate real part and imaginary part
-            rp <- -b/2a
-            ip <- root(-D)/2a
-            Display rp+j(ip) and rp-j(ip) as roots
-Step 5: Stop
+Step 2: Declare variables n, factorial and i.
+Step 3: Initialize variables
+        factorial <- 1
+        i <- 1
+Step 4: Read value of n
+Step 5: Repeat the steps until i = n
+     5.1: factorial <- factorial * i
+     5.2: i <- i + 1
+Step 6: Display factorial
+Step 7: Stop
 */
 
 #include <iostream>
