413f483e3a351d198c24e8e422822d2618e78835
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-04 06:22:47 +0600

    write sudo code of prime or not

--- DSA/DSA.cpp
+++ DSA/DSA.cpp
@@ -1,15 +1,20 @@
-// Algorithm 4: Find the factorial of a number
+// Algorithm 5: Check whether a number is prime or not
 /*
 Step 1: Start
-Step 2: Declare variables n, factorial and i.
+Step 2: Declare variables n, i, flag.
 Step 3: Initialize variables
-        factorial <- 1
-        i <- 1
-Step 4: Read value of n
-Step 5: Repeat the steps until i = n
-     5.1: factorial <- factorial * i
-     5.2: i <- i + 1
-Step 6: Display factorial
+        flag <- 1
+        i <- 2
+Step 4: Read n from the user.
+Step 5: Repeat the steps until i=(n/2)
+     5.1 If remainder of n/i equals 0
+            flag <- 0
+            Go to step 6
+     5.2 i <- i + 1
+Step 6: If flag = 0
+           Display n is not prime
+        else
+           Display n is prime
 Step 7: Stop
 */
 
@@ -18,16 +23,6 @@ Step 7: Stop
 using namespace std;
 
 int main() {
-  int n, factorial = 1, i = 1;
-
-  cin >> n;
-
-  while (i <= n) {
-    factorial *= i;
-    i++;
-  }
-
-  cout << "Factorial of " << n << " is " << factorial << endl;
 
   return 0;
 }
