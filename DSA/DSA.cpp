740d8306e0dc70a5f45ae66060106fa8c5a030e5
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-04 06:46:31 +0600

    write code for check a number is prime

--- DSA/DSA.cpp
+++ DSA/DSA.cpp
@@ -23,6 +23,22 @@ Step 7: Stop
 using namespace std;
 
 int main() {
+  int n, i, flag = 1;
 
-  return 0;
+  cin >> n;
+
+  for (i = 2; i <= n / 2; i++) {
+    if (n % i == 0) {
+      flag = 0;
+      break;
+    }
+  }
+
+  if (flag == 0) {
+    cout << n << " is not prime" << endl;
+  } else {
+    cout << n << " is prime" << endl;
+  }
+
+    return 0;
 }
