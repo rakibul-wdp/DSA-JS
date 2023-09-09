6a17b42f372fa39a2b80131ac968ed16a1eaabc5
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-09 08:39:28 +0600

    write sudo code of find the factorial of n

--- DSA/DSA.cpp
+++ DSA/DSA.cpp
@@ -1,24 +1,15 @@
-// Algorithm 6: Find the Fibonacci series till the term less than 1000
-
 /*
-Step 1: Start
-Step 2: Declare variables first_term, send_term and temp.
-Step 3: Initialize variables first_term <- 0 second_term <- 1
-Step 4: Display first_term and second_term
-Step 5: Repeat the steps until send_term <= 1000
-     5.1: tem <- second_term
-     5.2: second_term <- second_term + first_term
-     5.3: first_term <- temp
-     5.4: Display second_term
-Step 6: Stop
+Initialize fact = 1
+For every value v in range 1 to n:
+  Multiple the fact by v
+fact contains the factorial of n
 */
 
 #include <iostream>
-
 using namespace std;
 
 int main() {
-  }
-
+  
+  
   return 0;
 }
