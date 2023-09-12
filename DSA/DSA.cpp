a1da839445bd92e84a0d7ef2fe2b6085aa1e2955
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-13 04:26:16 +0600

    start to check if the stack is full for stack

--- DSA/DSA.cpp
+++ DSA/DSA.cpp
@@ -1,28 +1,28 @@
-// Algorithm (by Bob)
-/*
-Initialize sum = 0
-for every natural number n in range 1 to 10^11(inclusive):
-  add n to sum
-sum is your answer
-*/
+// Stack implementation in C++
 
+#include <stdlib.h>
 #include <iostream>
+
 using namespace std;
 
-int findSum() {
-  int sum = 0;
+#define MAX  10
+int size = 0;
 
-  for (int v = 1; v <= 10000000000; v++) {
-    sum += v;
-  }
+// Creating a stack
+struct stack {
+  int items[MAX];
+  int top;
+};
+typedef struct stack st;
 
-  return sum;
+void createEmptyStack(st *s) {
+  s->top = -1;
 }
 
-int main() {
-  int result = findSum();
-
-  cout << result;
-
-  return 0;
+// Check if the stack is full
+int isFull(st *s) {
+  if (s->top == MAX - 1)
+    return 1;
+  else
+    return 0;
 }
