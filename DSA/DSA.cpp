4acbccb5863a605808344bd5327cc66e539bc545
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-17 05:05:00 +0600

    create display function

--- DSA/DSA.cpp
+++ DSA/DSA.cpp
@@ -60,4 +60,21 @@ class Queue {
       return (element);
     }
   }
+
+  void display() {
+    int i;
+    if (isEmpty()) {
+      cout << endl
+        << "Empty Queue" << endl;
+    } else {
+      cout << endl
+           << "Front index-> " << front;
+      cout << endl
+           << "Items ->";
+      for (i = front; i <= rear; i++)
+        cout << items[i] << "  ";
+      cout << endl
+           << "Rear index-> " << rear << endl;
+    }
+  }
 }
