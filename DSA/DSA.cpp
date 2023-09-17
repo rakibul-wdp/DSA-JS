b225e70174159d9197f007b3b7219f4904dd7c49
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-18 04:05:00 +0600

    create main function of queue

--- DSA/DSA.cpp
+++ DSA/DSA.cpp
@@ -77,4 +77,26 @@ class Queue {
            << "Rear index-> " << rear << endl;
     }
   }
+};
+
+int main() {
+  Queue q;
+
+  q.deQueue();
+
+  q.enQueue(1);
+  q.enQueue(2);
+  q.enQueue(3);
+  q.enQueue(4);
+  q.enQueue(5);
+
+  q.enQueue(6);
+
+  q.display();
+
+  q.deQueue();
+
+  q.display();
+
+  return 0;
 }
