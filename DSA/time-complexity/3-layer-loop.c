e7813adf1840307b79057dfb908800547c7a50b4
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-26 13:00:18 +0600

    order of n cube time complexity

---
+++ DSA/time-complexity/3-layer-loop.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+  int i, j, k, n, count;
+
+  scanf("%d", &n);
+
+  count = 0;
+
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n; j++) {
+      for (k = 0; k < n; k++) {
+        count = count + 1;
+      }
+    }
+  }
+
+  printf("n = %d, count = %d\n", n, count);
+
+  return 0;
+}
+
+/*
+3 layer nested loop and every layer depend on n that's why time complexity is O(n^3)
+*/
