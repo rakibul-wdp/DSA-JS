9f690975a6d0531424a9220a5f334af9da18a28d
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-27 12:06:19 +0600

    create node

--- DSA/tree/tree.c
+++ DSA/tree/tree.c
@@ -7,3 +7,17 @@ struct node {
   Node *left;
   Node *right;
 };
+
+Node *create_node(int item) {
+  Node *new_node = (Node *)malloc(sizeof(Node));
+  if (new_node == NULL) {
+    printf("Error! Could Not Create A New Node\n");
+    exit(1);
+  }
+
+  new_node->data = item;
+  new_node->left = NULL;
+  new_node->right = NULL;
+
+  return new_node;
+}
