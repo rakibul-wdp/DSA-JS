6135130a8d8f1cd5775e7b2fbaa0ccfe09ced9e0
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-27 10:06:03 +0600

    dequeue

--- DSA/queue/queue-example.c
+++ DSA/queue/queue-example.c
@@ -13,3 +13,18 @@ void enqueue(Queue *q, int item) {
   q->data[q->tail] = item;
   q->tail = (q->tail + 1) % (Q_SIZE + 1);
 }
+
+int dequeue(Queue *q) {
+  int item;
+
+  if (q->tail == q->head) {
+    printf("Queue is empty\n");
+    return -1;
+  }
+
+  item = q->data[q->head];
+
+  q->head = (q->head + 1) % (Q_SIZE + 1);
+
+  return item;
+}
