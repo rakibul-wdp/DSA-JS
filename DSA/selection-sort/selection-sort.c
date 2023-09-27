73e5ab9962b01c7bd917b24f1a585658cca5299e
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-27 08:50:04 +0600

    insertion sort

--- DSA/selection-sort/selection-sort.c
+++ DSA/selection-sort/selection-sort.c
@@ -32,6 +32,21 @@ void bubble_sort(int A[], int n) {
   }
 }
 
+void insertion_sort(int A[], int n) {
+  int i, j, item;
+
+  for (i = 1; i < n; i++) {
+    item = A[i];
+
+    j = i - 1;
+    while (j >= 0 && A[j] > item) {
+      A[j + 1] = A[j];
+      j = j - 1;
+    }
+    A[j + 1] = item;
+  }
+}
+
 int main () {
   return 0;
 }
