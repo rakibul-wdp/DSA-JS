824ea59075d6b3ec591dbba7594153d134d4e381
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-27 08:27:41 +0600

    bubble sort

--- DSA/selection-sort/selection-sort.c
+++ DSA/selection-sort/selection-sort.c
@@ -18,6 +18,20 @@ void selection_sort(int A[], int n) {
   }
 }
 
+void bubble_sort(int A[], int n) {
+  int i, j, temp;
+
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n - i - 1; j++) {
+      if (A[j] > A[j+1]) {
+        temp = A[j];
+        A[j] = A[j + 1];
+        A[j + 1] = temp;
+      }
+    }
+  }
+}
+
 int main () {
   return 0;
 }
