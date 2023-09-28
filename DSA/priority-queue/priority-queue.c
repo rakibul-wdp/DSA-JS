aebf5fa33b8aa9f292b27b61297cafd465aa7f88
rakibul-wdp <rakibul.wdp@gmail.com>
2023-09-28 08:39:08 +0600

    create insert node

--- DSA/priority-queue/priority-queue.c
+++ DSA/priority-queue/priority-queue.c
@@ -11,3 +11,22 @@ int extract_max(int heap[], int heap_size) {
 
   return max_item;
 }
+
+int insert_node(int heap[], int heap_size, int node) {
+  int i, p, t;
+
+  heap_size += 1;
+  heap[heap_size] = node;
+
+  while (i > 1 && heap[i] > heap[parent(i)]) {
+    p = parent(i);
+
+    t = heap[p];
+    heap[p] = heap[i];
+    heap[i] = t;
+
+    i = p;
+  }
+
+  return heap_size;
+}
