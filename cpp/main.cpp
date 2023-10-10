026838cb24f04f495372fa14e178ae6c9dfe8277
rakibul-wdp <rakibul.wdp@gmail.com>
2023-10-11 01:39:00 +0600

    build in swap in cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <utility>
 using namespace std;
 
 int main()
@@ -6,8 +7,9 @@ int main()
   int a, b;
   cin >> a >> b;
 
-  cout << min(a, b) << endl
-       << max(a, b) << endl;
+  swap(a, b);
+
+  cout << a << b << endl;
 
   return 0;
 }
