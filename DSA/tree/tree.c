20fd813c1fb2a1d0c18da573dba4685d054afede
rakibul-wdp <rakibul.wdp@gmail.com>
2023-10-15 03:39:00 +0600

    binary tree postorder traversal

--- DSA/tree/tree.c
+++ DSA/tree/tree.c
@@ -57,6 +57,18 @@ void pre_order(Node *node) {
   }
 }
 
+void post_order(Node *node) {
+  if (node->left != NULL) {
+    post_order(node->left);
+  }
+
+  if (node->right != NULL) {
+    post_order(node->right);
+  }
+
+  printf("%d  ", node->data);
+}
+
 int main() {
   Node *root = create_tree();
 
