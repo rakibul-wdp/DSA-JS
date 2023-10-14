a868d18784e4e0a7e56c837ac9bef729a854b0ae
rakibul-wdp <rakibul.wdp@gmail.com>
2023-10-14 08:19:18 +0600

    dynamic memory

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -3,7 +3,14 @@ using namespace std;
 
 int main()
 {
-  int a;
+  int *a = new int;
+  *a = 10;
+
+  cout << *a << endl;
+
+  float *f = new float;
+  *f = 2.56;
+  cout << *f << endl;
 
   return 0;
 }
