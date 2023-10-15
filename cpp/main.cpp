fda3a38207858b4e7c5397e40fa55fe1e9aa1973
rakibul-wdp <rakibul.wdp@gmail.com>
2023-10-15 09:03:46 +0600

    sorting

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,33 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Student
-{
-public:
-  char name[100];
-  int roll;
-  int cls;
-  char section;
-
-  Student(int r, int s, int c, char *n)
-  {
-    roll = r;
-    section = s;
-    cls = c;
-    strcpy(name, n);
-  }
-};
-
-Student fun()
-{
-  char name[100] = "Abul";
-  Student abul(29, 'C', 7, name);
-  return abul;
-}
-
 int main()
 {
-  Student abul = fun();
+  int n;
+  cin >> n;
+  int arr[n];
+  for (int i = 0; i < n; i++)
+  {
+    cin >> arr[i];
+  }
+
+  // sorting
+  sort(arr, arr + n);
+
+  for (int i = 0; i < n; i++)
+  {
+    cout << arr[i] << "";
+  }
 
   return 0;
 }
