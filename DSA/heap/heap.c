d34f7364624941f3d5c1894f3364e653bf3a4490
rakibul-wdp <rakibul.wdp@gmail.com>
2023-10-18 02:39:00 +0600

    heap sort function created

--- DSA/heap/heap.c
+++ DSA/heap/heap.c
@@ -32,3 +32,16 @@ void build_max_heap(int heap[], int heap_size) {
     max_heapify(heap, heap_size, i);
   }
 }
+
+void heap_sort(int heap[], int heap_size) {
+  int i, t;
+
+  for (i = heap_size; i > 1; i--) {
+    t = heap[1];
+    heap[1] = heap[i];
+    heap[i] = t;
+
+    heap_size -= 1;
+    max_heapify(heap, heap_size, 1);
+  }
+}
