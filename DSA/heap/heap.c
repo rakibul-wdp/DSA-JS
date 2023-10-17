57ece05668148b65103ea9ad166ccc424260b697
rakibul-wdp <rakibul.wdp@gmail.com>
2023-10-18 01:39:00 +0600

    build max heap

--- DSA/heap/heap.c
+++ DSA/heap/heap.c
@@ -24,3 +24,11 @@ void max_heapify(int heap[], int heap_size, int i) {
     max_heapify(heap, heap_size, largest);
   }
 }
+
+void build_max_heap(int heap[], int heap_size) {
+  int i;
+
+  for (i = heap_size / 2; i >= 1; i--) {
+    max_heapify(heap, heap_size, i);
+  }
+}
