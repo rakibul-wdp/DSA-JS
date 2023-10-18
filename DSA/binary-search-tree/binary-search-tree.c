fe7367b9bedbb885f3278e85acf5b32fda22b66d
rakibul-wdp <rakibul.wdp@gmail.com>
2023-10-19 01:39:00 +0600

    create bst delete function

--- DSA/binary-search-tree/binary-search-tree.c
+++ DSA/binary-search-tree/binary-search-tree.c
@@ -48,3 +48,22 @@ Node *bst_transplant(Node *root, Node *current_node, Node *new_node) {
 
   return root;
 }
+
+Node *bst_delete(Node *root, Node *node) {
+  Node *smallest_node;
+  if (node->left == NULL) {
+    root = bst_transplant(root, node, node->right);
+  } else if (node->right == NULL) {
+    root = bst_transplant(root, node, node->left);
+  } else {
+    smallest_node = bst_minimum(node->right);
+    if (smallest_node->parent != node) {
+      root = bst_transplant(root, smallest_node, smallest_node->right);
+      add_right_child(smallest_node, node->right);
+    }
+    root = bst_transplant(root, node, smallest_node);
+    add_left_child(smallest_node, node->left);
+  }
+  free(node);
+  return root;
+}
