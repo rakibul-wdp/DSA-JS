c40b5b19020b4530b8fde5868bbafb2396c4ac6a
rakibul-wdp <rakibul.wdp@gmail.com>
2023-10-19 02:39:00 +0600

    write hello world for fixing an issue

--- DSA/dsa.c
+++ DSA/dsa.c
@@ -1,22 +1,7 @@
 #include <stdio.h>
 
 int main() {
-  int i, j, n, count;
-
-  scanf("%d", &n);
-
-  count = 0;
-  for (i = 0; i < n; i++) {
-    for (j = 0; j < n; j++) {
-      count = count + 1;
-    }
-  }
-
-  for (i = 0; i < n; i++) {
-    count = count + 1;
-  }
-
-    printf("count = %d\n", count);
+  printf("Hello World C!");
 
   return 0;
 }
