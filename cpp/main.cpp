81add855ccd5660dc562213c20f84c576985343e
rakibul-wdp <rakibul.wdp@gmail.com>
2023-10-26 15:17:33 +0600

    clean code

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -4,12 +4,8 @@ using namespace std;
 int main()
 {
   string s;
-  cin >> s;
-  cout << s[0] << endl;
-  cout << s.at(0) << endl;
-  cout << s[s.size() - 1] << endl;
-  cout << s.back() << endl;
-  cout << s.front() << endl;
 
   return 0;
 }
+
+// completed module 5
