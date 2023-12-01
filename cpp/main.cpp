e2a8df10cc88625dbc0250e96b69a1fe4dae2741
rakibul-wdp <rakibul.wdp@gmail.com>
2023-12-02 03:39:00 +0600

    string element access

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main()
 {
-  string str = "Hello World";
-
-  cout << str.size() << endl;
-  cout << str.max_size() << endl;
-  cout << str.max_size() << endl;
-  // str.clear();
-  // cout << str.empty() << endl;
+  string s;
+  cin >> s;
+  cout << s[0] << endl;
+  cout << s.at(0) << endl;
+  cout << s[s.size() - 1] << endl;
+  cout << s.back() << endl;
+  cout << s.front() << endl;
 
   return 0;
 }
