aada0d4507a6ef8edabac4b8fcb7514a120c0647
rakibul-wdp <rakibul.wdp@gmail.com>
2024-08-08 08:42:23 +0600

    recursive fibonacci problem

---
+++ algorithm/12-recursive-fibonacci-sequence.js
@@ -0,0 +1,15 @@
+/*
+Recursive Fibonacci sequence
+
+Problem: Give a number 'n', find the n'th element of the Fibonacci sequence
+
+In mathematics, the Fibonacci sequence is a sequence in which each number is the sum of the two preceding ones.
+
+The first two numbers in the sequence are 0 and 1. (0, 1, 1, 2, 3, 5, 8...)
+
+recursiveFibonacci(0) = 0
+recursiveFibonacci(1) = 1
+recursiveFibonacci(6) = 8
+*/
+
+function fibonacci(n) {}
