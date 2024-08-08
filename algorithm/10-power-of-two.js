367fdd9ea4b079e6c147125b56ff3c6d72840c95
rakibul-wdp <rakibul.wdp@gmail.com>
2024-08-08 06:12:33 +0600

    power of two library and normal

---
+++ algorithm/10-power-of-two.js
@@ -0,0 +1,26 @@
+/*
+Power of Two
+
+Problem: Give a positive integer 'n', determine if the number is a power of 2 or not. An integer is power of two if there exists an integer 'x' such that 'n' === 2^x
+
+isPowerOfTwo(1) = true(2^0)
+isPowerOfTwo(2) = true(2^1)
+isPowerOfTwo(5) = false
+*/
+
+function isPowerOfTwo(n) {
+  if (n < 1) return false;
+
+  while (n > 1) {
+    if (n % 2 !== 0) return false;
+    n /= 2;
+  }
+
+  return true;
+}
+
+function isPowerOfTwoAlt(n) {
+  return Number.isInteger(Math.sqrt(n));
+}
+
+// Big-O - O(logn)
--- algorithm/4-big-O-notation.md
+++ algorithm/4-big-O-notation.md
@@ -18,3 +18,10 @@
   }
   ```
 - n + 2 | It is expressed in terms of the input
+
+### Big-O Guide (Cheat sheet)
+
+- Calculation not dependent on input size - O(1)
+- 1 loop - O(n)
+- 2 nested loops - O(n^2)
+- Input size reduced by half - O(logn)
