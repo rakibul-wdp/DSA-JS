0396db230f6b6d8d4b7d10d9a2bb3780b8631ef9
rakibul-wdp <rakibul.wdp@gmail.com>
2024-08-10 09:23:42 +0600

    binary search problem

---
+++ algorithm/16-binary-search.js
@@ -0,0 +1,9 @@
+/*
+Binary search
+
+Problem: Given a sorted array of 'n' elements and a target element 't', find the index of 't' in the array. Return -1 if the target element is not found.
+
+arr = [-5, 2, 4, 6, 10], t = 10 -> should return 4
+*/
+
+function findIndex(arr, t) {}
