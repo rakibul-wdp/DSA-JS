3f7f1ca7eea5ad689193c2e6c99f1c3bc8aa09fe
rakibul-wdp <rakibul.wdp@gmail.com>
2024-08-14 07:06:55 +0600

    closure by function

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
-/**
- * @param {number} n
- * @return {Function} counter
- */
-
-var createCounter = function (n) {
-  let i = n;
-  return function () {
-    return (i += 1);
+function createAdder(a) {
+  return function add(b) {
+    const sum = a + b;
+    return sum;
   };
-};
+}
 
-const counter = createCounter(3);
-console.log(counter());
-console.log(counter());
-console.log(counter());
-
-/**
- * const counter = createCounter(10)
- * counter() // 10
- * counter() // 11
- * counter() // 12
- */
+const res = createAdder(2);
+console.log(res(3));
