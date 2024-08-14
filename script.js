2b38d1898b4c13851d9053dde124ef42f09c5869
rakibul-wdp <rakibul.wdp@gmail.com>
2024-08-14 06:43:01 +0600

    counter

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function f(...args) {
-  console.log(args);
-  const sum = args[0] + args[1];
-  return sum;
-}
-console.log(f(3, 4));
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+
+var createCounter = function (n) {
+  let i = n;
+  return function () {
+    return (i += 1);
+  };
+};
+
+const counter = createCounter(3);
+console.log(counter());
+console.log(counter());
+console.log(counter());
+
+/**
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
